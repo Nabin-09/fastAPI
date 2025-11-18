4eb35975ba3c7317b2bed1d00f64e5138f2d7cef
Nabin <sharmanabin.0910@gmail.com>
2025-11-18 23:26:47 +0530

    Query params - desc and how to use

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Path , HTTPException
+from fastapi import FastAPI , Path , HTTPException , Query
 import json 
 
 app = FastAPI()
@@ -35,4 +35,20 @@ def view_patient(patient_id : str = Path(... ,description='ID of the patient in 
 
 
 @app.get('/sort')
-def sort_patient(sort_by: str)
+def sort_patient(sort_by: str = Query(... , description='Sort on basis of height , weight or BMI?'),
+    order:str = Query('asc' , description='Sort in ascending or desceinding order ?')):
+
+    valid_fields = ['height' , 'weight' , 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid Field select from {valid_fields}')
+
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400 , detail = 'The order must be either asc or desc')
+    
+    data  = load_data()
+
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(data.values() , key=lambda x : x.get(sort_by , 0), reverse=sort_order) 
+
+    return sorted_data
