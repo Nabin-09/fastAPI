4f581183bb27cf2b995ded54e45490ea3ce49eef
Nabin <sharmanabin.0910@gmail.com>
2025-11-18 13:20:28 +0530

    Rendering data from a JSON file

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+import json 
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json' , 'r') as f:
+        data = json.load(f)
+    return data
+
+
 @app.get("/")
 def hello():
-    return {'message':'Namaste Dunia'}
+    return {'message':'Patient management APi'}
 
 @app.get('/about')
 def about():
-    return {'message' : "This is Nabin's about"}
+    return {'message' : "This is generation of suntheitc data"}
+
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
